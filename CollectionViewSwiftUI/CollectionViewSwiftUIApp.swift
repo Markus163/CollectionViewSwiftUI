2ddbcd18b1fb55bc5fa3c0f713bb784c5f5f6fda
Markus163 <pprimak@mail.ru>
2023-08-13 11:55:00 +0400

    Initial Commit

---
+++ CollectionViewSwiftUI/CollectionViewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewSwiftUIApp.swift
+//  CollectionViewSwiftUI
+//
+//  Created by Марк Михайлов on 13.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct CollectionViewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
